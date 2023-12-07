ad9ca76032a0871a529d58dfec4c705a1d0fcdfb
atulsingh369 <businessatul7684@gmail.com>
2023-12-08 00:33:01 +0530

    Updated UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,50 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="flex justify-stretch overflow-hidden w-screen h-3 items-center">
+          <span className="w-1/5 h-full bg-[#ff671f]"></span>
+          <span className="w-1/5 h-full bg-[#000081]"></span>
+          <span className="w-1/5 h-full bg-[#da1d00]"></span>
+          <span className="w-1/5 h-full bg-[#4bc9fc]"></span>
+          <span className="w-1/5 h-full bg-[#046a38]"></span>
+        </div>
+        <nav className="grid items-center h-fit grid-cols-6 gap-x-4 font-bold text-xl">
+          <Link href="/">
+            <Image
+              src="https://ik.imagekit.io/e5ixuxrlb/esm/logo.png?updatedAt=1685270347657"
+              alt="logo"
+              className="mx-auto"
+              width={125}
+              height={125}
+            />
+          </Link>
+          <span>ABOUT US</span>
+          <span>MEMBERS</span>
+          <span>GALLERY</span>
+          <span>NOTICES</span>
+          <button className="border-2 border-black rounded-md p-2 w-fit ">
+            SIGN UP
+          </button>
+        </nav>
+        {children}
+        <footer className="fixed bottom-0 bg-[#323e48] text-white">
+          <div className="flex justify-stretch overflow-hidden w-screen h-3 items-center">
+            <span className="w-1/5 h-full bg-[#ff671f]"></span>
+            <span className="w-1/5 h-full bg-[#000081]"></span>
+            <span className="w-1/5 h-full bg-[#da1d00]"></span>
+            <span className="w-1/5 h-full bg-[#4bc9fc]"></span>
+            <span className="w-1/5 h-full bg-[#046a38]"></span>
+          </div>
+
+          <span className="flex justify-center items-center my-2 text-2xl">
+            भूतपूर्व सैनिक जन कल्याण समिति उ.प्र.
+          </span>
+          <span className="flex justify-center items-center my-2 text-2xl">
+            ❤️ Built by Atul Singh ❤️
+          </span>
+        </footer>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center h-screen w-screen relative text-white bg-black space-y-6">
-      <p className="text-5xl">भूतपूर्व सैनिक जन कल्याण समिति, उ.प्र .</p>
-      <p className="text-2xl">Site Under Construction...</p>
-      <div className="fixed bottom-4 text-2xl text-center">
-        ❤️ Built by Atul Singh ❤️
-      </div>
+    <div className="flex flex-col justify-center items-center relative space-y-6">
+      
     </div>
   );
 }
